04b78308d3fd265db1f87011762ecccdc04672a9
talyh <taly.hotimsky@gmail.com>
2018-02-10 12:46:25 -0500

    Initial assets

---
+++ Assets/Agent.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(MeshRenderer))]
+public class Agent : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _redPost;
+    [SerializeField]
+    private Transform _bluePost;
+
+    [SerializeField]
+    private Material _redTeam;
+    [SerializeField]
+    private Material _blueTeam;
+    [SerializeField]
+    private Material _defaultMaterial;
+
+    [SerializeField]
+    private float _linearMaxSpeed;
+    [SerializeField]
+    private float _angularMaxSpeed;
+    [SerializeField]
+    private float _linearAcceleration;
+    [SerializeField]
+    private float _angularAcceleration;
+
+    [SerializeField]
+    private Rigidbody _rb;
+    [SerializeField]
+    private MeshRenderer _meshRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DetermineAgentColor();
+    }
+
+    void DetermineAgentColor()
+    {
+        float distanceToRedPost = Vector3.Distance(transform.position, _redPost.position);
+        float distanceToBluePost = Vector3.Distance(transform.position, _bluePost.position);
+
+        if (Mathf.Abs(distanceToBluePost - distanceToRedPost) < 5)
+        {
+            SetMaterial(_defaultMaterial);
+        }
+        else if (distanceToBluePost > distanceToRedPost)
+        {
+            SetMaterial(_redTeam);
+        }
+        else
+        {
+            SetMaterial(_blueTeam);
+        }
+    }
+
+    void SetMaterial(Material newMaterial)
+    {
+        _meshRenderer.material = newMaterial;
+    }
+
+    public void MoveForwards()
+    {
+        _rb.velocity = transform.forward * _linearMaxSpeed;
+    }
+
+    public void MoveBackwards()
+    {
+        _rb.velocity = -transform.forward * _linearMaxSpeed;
+    }
+
+    public void StrafeRight()
+    {
+        _rb.velocity = transform.right * _linearMaxSpeed;
+    }
+
+    public void StrafeLeft()
+    {
+        _rb.velocity = -transform.right * _linearMaxSpeed;
+    }
+
+    public void TurnRight()
+    {
+        _rb.angularVelocity = transform.up * _angularMaxSpeed;
+    }
+
+    public void TurnLeft()
+    {
+        _rb.angularVelocity = -transform.up * _angularMaxSpeed;
+    }
+
+    public void StopMoving()
+    {
+        _rb.velocity = Vector3.zero;
+    }
+
+    public void StopTurning()
+    {
+        _rb.angularVelocity = Vector3.zero;
+    }
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Agent _agent;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!_agent)
+        {
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            _agent.MoveForwards();
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _agent.StrafeLeft();
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            _agent.MoveBackwards();
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            _agent.StrafeRight();
+        }
+        if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.LeftShift))
+        {
+            _agent.TurnLeft();
+        }
+        if (Input.GetKey(KeyCode.E) || Input.GetKey(KeyCode.RightShift))
+        {
+            _agent.TurnRight();
+        }
+
+        if (Input.GetKeyUp(KeyCode.W)
+            || Input.GetKeyUp(KeyCode.UpArrow)
+            || Input.GetKeyUp(KeyCode.A)
+            || Input.GetKeyUp(KeyCode.LeftArrow)
+            || Input.GetKeyUp(KeyCode.S)
+            || Input.GetKeyUp(KeyCode.DownArrow)
+            || Input.GetKeyUp(KeyCode.Q)
+            || Input.GetKeyUp(KeyCode.RightArrow)
+            )
+        {
+            _agent.StopMoving();
+        }
+        if (Input.GetKeyUp(KeyCode.Q)
+            || Input.GetKeyUp(KeyCode.LeftShift)
+            || Input.GetKeyUp(KeyCode.E)
+            || Input.GetKeyUp(KeyCode.RightShift)
+            )
+        {
+            _agent.StopTurning();
+        }
+    }
+}
